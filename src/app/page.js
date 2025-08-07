04124fb4ae07f2f49736739f5f38b71fdd141521
ReatorGee <renato.garcia@utec.edu.pe>
2025-08-06 21:05:19 -0500

    analytics import

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 // src/app/page.js
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <main style={{ textAlign: 'center', marginTop: '5rem' }}>
-      <h1>broer</h1>
+      <h1>salchipollo o salchiburger</h1>
       <p>broski</p>
     </main>
   );
